a2d71a7a10d6f7ed8720651af3e659f646d258c9
Daniel Escanez-Exposito <jescanez@ull.edu.es>
2025-12-04 16:26:13 +0100

    Day 03 part 2

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -10,25 +10,34 @@ fn main() {
 
     let banks = banks_string.split('\n').map(|bank| {
         bank.chars()
-            .map(|joltage| joltage.to_digit(10).expect("Invalid joltage"))
+            .map(|joltage| joltage.to_digit(10).expect("Invalid joltage") as u64)
             .enumerate()
-            .collect::<Vec<(usize, u32)>>()
+            .collect::<Vec<(usize, u64)>>()
     });
+
+    const MAX_TURNED_BATTERIES: usize = 12;
 
     let result = banks
         .map(|bank| {
-            let bank_without_last_joltage = bank.iter().rev().skip(1);
-            let (position_best, best_joltage) = bank_without_last_joltage
-                .max_by_key(|&(_, joltage)| joltage)
-                .unwrap();
-            let (_, second_best_joltage) = bank
-                .iter()
-                .filter(|&(i, _)| i > position_best)
-                .max_by_key(|&(_, joltage)| joltage)
-                .unwrap();
-            10 * best_joltage + second_best_joltage
+            (0..MAX_TURNED_BATTERIES).fold((0, bank), |acc, turned_batteries| {
+                let current_bank = acc.1.clone();
+                let (position_best, best_joltage) = current_bank
+                    .into_iter()
+                    .rev()
+                    .skip(MAX_TURNED_BATTERIES - (1 + turned_batteries))
+                    .max_by_key(|&(_, joltage)| joltage)
+                    .unwrap();
+                let next_bank = acc
+                    .1
+                    .into_iter()
+                    .filter(|(i, _)| i > &position_best)
+                    .collect::<Vec<_>>();
+
+                (10 * acc.0 + best_joltage, next_bank)
+            })
         })
-        .sum::<u32>();
+        .map(|(joltage, _)| joltage)
+        .sum::<u64>();
 
     println!("Result: {}", result);
 }
