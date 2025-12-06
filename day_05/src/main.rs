364eab61c44fd7cb51748390e9753e9b95e2daaf
Daniel Escanez-Exposito <jescanez@ull.edu.es>
2025-12-06 18:53:46 +0100

    Day 05 part 2

--- day_05/src/main.rs
+++ day_05/src/main.rs
@@ -1,7 +1,8 @@
 use core::panic;
 use std::fs;
 
-type Id = u64;
+type Id = usize;
+#[derive(Clone, Copy, PartialEq, PartialOrd, Ord, Eq, Debug)]
 struct RangeId {
     lower: Id,
     upper: Id,
@@ -11,8 +12,8 @@ impl RangeId {
     pub fn new(line: &str) -> RangeId {
         let [lower, upper] = line
             .split('-')
-            .map(|limit| limit.parse::<u64>().expect("Invalid ID in range"))
-            .collect::<Vec<u64>>()[0..=1]
+            .map(|limit| limit.parse::<Id>().expect("Invalid ID in range"))
+            .collect::<Vec<Id>>()[0..=1]
         else {
             panic!("Invalid range")
         };
@@ -31,9 +32,24 @@ impl Cafeteria {
         else {
             panic!("Invalid cafeteria format")
         };
-        let ranges = ranges
+        let mut raw_ranges = ranges
             .split('\n')
             .map(RangeId::new)
+            .collect::<Vec<RangeId>>();
+        raw_ranges.sort();
+
+        let ranges = raw_ranges
+            .clone()
+            .into_iter()
+            .filter(|&range| {
+                !raw_ranges.iter().any(|&compared_range| {
+                    compared_range != range
+                        && compared_range.lower <= range.lower
+                        && range.lower <= compared_range.upper
+                        && compared_range.lower <= range.upper
+                        && range.upper <= compared_range.upper
+                })
+            })
             .collect::<Vec<RangeId>>();
         let available_ids = available_ids
             .split('\n')
@@ -45,7 +61,7 @@ impl Cafeteria {
         }
     }
 
-    pub fn count_fresh(&self) -> usize {
+    pub fn count_available_fresh(&self) -> usize {
         self.available_ids
             .iter()
             .filter(|&id| {
@@ -54,6 +70,37 @@ impl Cafeteria {
                     .any(|range| range.lower <= *id && id <= &range.upper)
             })
             .count()
+    }
+
+    pub fn count_range_fresh(&self) -> usize {
+        let ranges_to_compare = (0..self.ranges.len()).map(|i| {
+            (
+                self.ranges[i],
+                self.ranges
+                    .iter()
+                    .enumerate()
+                    .filter(move |&(j, _)| i < j)
+                    .map(|(_, range)| range),
+            )
+        });
+
+        ranges_to_compare
+            .into_iter()
+            .map(|(range, rest)| {
+                let upper = rest
+                    .filter(|compared_range| {
+                        compared_range.lower <= range.upper && range.upper <= compared_range.upper
+                    })
+                    .map(|container_range| container_range.lower - 1)
+                    .min()
+                    .unwrap_or(range.upper);
+                RangeId {
+                    lower: range.lower,
+                    upper,
+                }
+            })
+            .map(|range| range.upper + 1 - range.lower)
+            .sum()
     }
 }
 
@@ -66,6 +113,8 @@ fn main() {
         fs::read_to_string(input_filename).expect("Should have been able to read the file");
 
     let cafeteria = Cafeteria::new(cafeteria_string);
-    let result = cafeteria.count_fresh();
-    println!("Result: {}", result);
+    let result_part_1 = cafeteria.count_available_fresh();
+    let result_part_2 = cafeteria.count_range_fresh();
+    println!("Result part 1: {}", result_part_1);
+    println!("Result part 2: {}", result_part_2);
 }
